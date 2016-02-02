6cf0274041759b7d9ab0ca53c36727720ec35575
Jeannie Chiem <gnyorly@gmail.com>
2016-02-02 00:02:55 -0800

    Complete
    I'm tired.

--- eval.cpp
+++ eval.cpp
@@ -3,26 +3,32 @@
 #include <string>
 #include <stack>
 #include <cctype>
-#include <cassert>
 using namespace std;
 
 int evaluate(string infix, const Map& values, string& postfix, int& result)
 {
+	if (infix == "")
+		return 1;
+	postfix = "";
+	stack<char> op;
+	stack<int> opand;
 	int let = 0;
 	int paren = 0;
-	for (int i = 0; i < infix.size(); i++)	// Evaluates an integer arithmetic expression
+	for (unsigned int i = 0; i < infix.size(); i++)	
 	{
-		if (infix[i] == ' ')                
+		if (infix[i] == ' ')
 			continue;
 		if (let == 0)
 		{
 			if (islower(infix[i]))
 			{
+				postfix += infix[i];
 				let++;
 				continue;
 			}
 			else if (infix[i] == '(')
 			{
+				op.push('(');
 				paren++;
 				continue;
 			}
@@ -34,38 +40,80 @@ int evaluate(string infix, const Map& values, string& postfix, int& result)
 			{
 			case '+':
 			case '-':
+				while (!op.empty() && op.top() != '(')
+				{
+					postfix += op.top();
+					op.pop();
+				}
+				op.push(infix[i]);
+				let--;
+				break;
 			case '/':
 			case '*':
+				while (!op.empty() && op.top() != '(' && op.top() != '+' && op.top() != '-')
+				{
+					postfix += op.top();
+					op.pop();
+				}
+				op.push(infix[i]);
 				let--;
 				break;
 			case ')':
+				while (!op.empty() && op.top() != '(')
+				{
+					postfix += op.top();
+					op.pop();
+				}
+				if (!op.empty())
+					op.pop();
 				paren--;
 				break;
 			default:
 				return 1;
 			}
 	}
-	if (let == 1 && paren == 0)     //if syntactically valid
+	while (!op.empty())
 	{
-
+		postfix += op.top();
+		op.pop();
 	}
-// Precondition: infix is an infix integer arithmetic
-//   expression consisting of single lower case letter operands,
-//   parentheses, and the operators +, -, *, /, with embedded blanks
-//   allowed for readability. 
-// Postcondition: If infix is a syntactically valid infix integer
-//   expression whose only operands are single lower case letters
-//   (whether or not they appear in the values map), then postfix is
-//   set to the postfix form of the expression; otherwise postfix may
-//   or may not be changed, result is unchanged, and the function
-//   returns 1.  If infix is syntactically valid but contains at
-//   least one lower case letter operand that does not appear in the
-//   values map, then result is unchanged and the function returns 2.
-//   If infix is syntactically valid and all its lower case operand
-//   letters appear in the values map, then if evaluating the
-//   expression (using for each letter in the expression the value in
-//   the map that corresponds to it) attempts to divide by zero, then
-//   result is unchanged and the function returns 3; otherwise,
-//   result is set to the value of the expression and the function
-//   returns 0.
+	if (let != 1 || paren != 0)
+		return 1;
+	for (unsigned int x = 0; x < postfix.size(); x++)
+	{
+		if (islower(postfix[x]))
+		{
+			int temp;
+			if (!values.contains(postfix[x]))
+				return 2;
+			values.get(postfix[x], temp);
+			opand.push(temp);
+		}
+		else
+		{
+			int two = opand.top();
+			opand.pop();
+			int one = opand.top();
+			opand.pop();
+			switch (postfix[x])
+			{
+			case '+':
+				opand.push(one + two);
+				break;
+			case '-':
+				opand.push(one - two);
+				break;
+			case '*':
+				opand.push(one * two);
+				break;
+			case '/':
+				if (two == 0)
+					return 3;
+				opand.push(one / two);
+				break;
+			}
+		}
+	}
+	result = opand.top();
+	return 0;
 }
